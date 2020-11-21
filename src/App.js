8f3c01ac7106b1ab2ee979711d75160cd1f5bb41
SamithaN <samithaneranjan23@gmail.com>
2020-11-21 07:38:45 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+//import logo from './logo.svg';
+//import './App.css';
+import Notetaking from './component/notetaking';
+import React, { Component } from 'react';
+import './component/noteTaking.css';
+
+class App extends Component {
+
+
+  render(){
+  return (
+    <div className="newreact">
+      <Notetaking/>
+    </div>
+  );
+  }
+}
+
+
+export default App;
